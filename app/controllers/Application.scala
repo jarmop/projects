837c8c2352441ff4698e024223de8d689f30ff96
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-05-10 22:59:44 +0300

    Post test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
+import models.{Tax}
 import play.api._
+import play.api.data._
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
+
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Verotutka"))
+  }
+
+  val taxForm = Form(
+    tuple(
+      "salary" -> text,
+      "municipality" -> text,
+      "age" -> number
+    )
+  )
+
+  def calculate = Action { implicit request =>
+    val (salary, municipality, age) = taxForm.bindFromRequest.get
+    Ok(salary + municipality + age)
+    //Redirect(routes.Application.index)
   }
 
 }
---
+++ app/models/tax.scala
@@ -0,0 +1,9 @@
+package models
+
+import play.api.libs.json._
+
+class Tax(salary: Float, municipality: String, age: Int) {
+
+  def getTax = {}
+}
+
