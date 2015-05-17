3558cdb1df287a7c85e6183b9ac881013bcaf0d5
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-05-17 11:56:34 +0300

    Calculate correct government tax

--- app/models/GovernmentTax.scala
+++ app/models/GovernmentTax.scala
@@ -1,10 +1,63 @@
 package models
 
 import play.api.libs.json.{Json, Writes}
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
 
 case class GovernmentTax(salary: Int, naturalDeduction: Double, commonDeduction: Double) {
+  var tax: Double = -1
+  var taxSectionHits = new ListBuffer[Map[String, Double]]
+
   def getTax(): Double = {
-    98568
+    if (this.tax < 0)
+      this.tax = this.calculateTax()
+
+    this.tax
+  }
+
+  private def calculateTax(): Double = {
+    var tax: Double = if (this.salary < 165000) 0 else 800
+
+    val governmentTaxList = List[Map[String, Double]](
+      Map[String, Double]("minSalary" -> 1650000, "maxSalary" -> 2470000, "taxPercent" -> 0.065),
+      Map[String, Double]("minSalary" -> 2470000, "maxSalary" -> 4030000, "taxPercent" -> 0.175),
+      Map[String, Double]("minSalary" -> 4030000, "maxSalary" -> 7140000, "taxPercent" -> 0.215),
+      Map[String, Double]("minSalary" -> 7140000, "maxSalary" -> 9000000, "taxPercent" -> 0.2975),
+      Map[String, Double]("minSalary" -> 9000000, "maxSalary" -> 0, "taxPercent" -> 0.3175)
+    )
+
+    this.taxSectionHits = new ListBuffer[Map[String, Double]]
+    breakable { for (governmentTaxMap <- governmentTaxList) {
+      var salary = this.getDeductedSalary
+      if (salary < governmentTaxMap.get("minSalary").get)
+        break
+
+      if (governmentTaxMap.get("maxSalary").get > 0 && salary > governmentTaxMap.get("maxSalary").get)
+        salary = governmentTaxMap.get("maxSalary").get
+
+      var taxedSalary = salary - governmentTaxMap.get("minSalary").get;
+      val sectionTax = taxedSalary * governmentTaxMap.get("taxPercent").get;
+
+      tax += sectionTax;
+
+      this.taxSectionHits.append(Map[String, Double](
+        "tax" -> sectionTax,
+        "taxedSalary" -> taxedSalary,
+        "minSalary" -> governmentTaxMap.get("minSalary").get,
+        "maxSalary" -> governmentTaxMap.get("maxSalary").get,
+        "taxPercent" -> governmentTaxMap.get("taxPercent").get
+      ))
+    }}
+
+    tax;
+  }
+
+  private def getDeductedSalary: Double = {
+    this.salary - this.getDeduction
+  }
+
+  private def getDeduction: Double = {
+    this.commonDeduction
   }
 }
 
