4c26d22d52dac3d77baec25d2a0f21ca3a45ffc3
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-05-27 23:17:49 +0300

    Added several salaries to tax tests

--- test/TestSpec.scala
+++ test/TestSpec.scala
@@ -5,39 +5,86 @@ import org.junit.runner._
 
 @RunWith(classOf[JUnitRunner])
 class TestSpec extends Specification {
-  val salary = 3000000
   val municipality = "Helsinki"
   val age = 30
 
-  val tax = new Tax(salary, municipality, age)
+  val taxes = Map[Int,Map[String, Double]](
+    100000 -> Map(
+      "government" -> 0.0,
+      "municipality" -> 0.0,
+      "YLETax" -> 0.0,
+      "MedicalCareInsurancePayment" -> 0.0,
+      "perDiemPayment" -> 780.0,
+      "pensionContribution" -> 5700.0,
+      "unemploymentInsurance" -> 650.0,
+      "churchTax" -> 0.0,
+      "totalTax" -> 780.0
+    ),
+    1000000 -> Map(
+      "government" -> 0.0,
+      "municipality" -> 0.0,
+      "YLETax" -> 6378.4,
+      "MedicalCareInsurancePayment" -> 0.0,
+      "perDiemPayment" -> 7800.0,
+      "pensionContribution" -> 57000.0,
+      "unemploymentInsurance" -> 6500.0,
+      "churchTax" -> 0.0,
+      "totalTax" -> 14178.4
+    ),
+    3000000 -> Map(
+      "government" -> 98567.5,
+      "municipality" -> 447223.05365033617,
+      "YLETax" -> 14300.0,
+      "MedicalCareInsurancePayment" -> 31909.969233429398,
+      "perDiemPayment" -> 23400.0,
+      "pensionContribution" -> 171000.0,
+      "unemploymentInsurance" -> 19500.0,
+      "churchTax" -> 24174.21911623439,
+      "totalTax" -> 541007.242
+    ),
+    10000000 -> Map(
+      "government" -> 1620537.5,
+      "municipality" -> 1706625.0,
+      "YLETax" -> 14300.0,
+      "MedicalCareInsurancePayment" -> 121770.0,
+      "perDiemPayment" -> 78000.0,
+      "pensionContribution" -> 570000.0,
+      "unemploymentInsurance" -> 65000.0,
+      "churchTax" -> 92250.0,
+      "totalTax" -> 3610638.5
+    )
+  )
 
-  "Tax" should {
-    "government" in {
-      tax.getGovernmentTax equals(98567.5)
-    }
-    "municipality" in {
-      tax.getMunicipalityTax equals(447223.05365033617)
-    }
-    "YLETax" in {
-      tax.getYleTax equals(14300.0)
-    }
-    "MedicalCareInsurancePayment" in {
-      tax.getMedicalCareInsurancePayment equals(31909.969233429398)
-    }
-    "perDiemPayment" in {
-      tax.getPerDiemPayment equals(23400.0)
-    }
-    "pensionContribution" in {
-      tax.getPensionContribution equals(171000.0)
-    }
-    "unemploymentInsurance" in {
-      tax.getUnemploymentInsurance equals(19500.0)
-    }
-    "churchTax" in {
-      tax.getChurchTax equals(24174.21911623439)
-    }
-    "total tax" in {
-      tax.getTotalTax equals(541007.242)
+  for ((k,v) <- taxes) {
+    var tax = new Tax(k, municipality, age)
+    "Tax " + k should {
+      "government" in {
+        tax.getGovernmentTax equals(v.get("government").get)
+      }
+      "municipality" in {
+        tax.getMunicipalityTax.pp equals(v.get("municipality").get)
+      }
+      "YLETax" in {
+        tax.getYleTax equals(v.get("YLETax").get)
+      }
+      "MedicalCareInsurancePayment" in {
+        tax.getMedicalCareInsurancePayment.pp equals(v.get("MedicalCareInsurancePayment").get)
+      }
+      "perDiemPayment" in {
+        tax.getPerDiemPayment.pp equals(v.get("perDiemPayment").get)
+      }
+      "pensionContribution" in {
+        tax.getPensionContribution equals(v.get("pensionContribution").get)
+      }
+      "unemploymentInsurance" in {
+        tax.getUnemploymentInsurance equals(v.get("unemploymentInsurance").get)
+      }
+      "churchTax" in {
+        tax.getChurchTax.pp equals(v.get("churchTax").get)
+      }
+      "totalTax" in {
+        tax.getTotalTax equals(v.get("totalTax").get)
+      }
     }
   }
 }
