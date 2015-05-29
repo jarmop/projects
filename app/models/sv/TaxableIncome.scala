755ef3abd1d2efbb385c52a534a57f2b5986b113
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-05-29 23:38:41 +0300

    sv municipality: Calculate non taxable

--- app/models/sv/TaxableIncome.scala
+++ app/models/sv/TaxableIncome.scala
@@ -1,15 +1,17 @@
 package models.sv
 
+import play.api.Logger
 import play.api.libs.json.{Json, JsObject}
+import scala.math.floor
 
 class TaxableIncome(earnedIncome: Double) {
   var sum: Double = -1
+  var nonTaxable: Double = -1
 
   def getSum: Double = {
     if (this.sum < 0) {
       this.sum = this.calculateSum
     }
-
     this.sum
   }
 
@@ -26,7 +28,44 @@ class TaxableIncome(earnedIncome: Double) {
   }
 
   def getNonTaxable: Double = {
-    13100
+    if (this.nonTaxable < 0) {
+      this.nonTaxable = this.calculateNonTaxable
+    }
+    this.nonTaxable
+  }
+
+  def calculateNonTaxable: Double = {
+    if (this.earnedIncome < 18700) {
+      return 0
+    }
+    val minimumNonTaxable: Double = 18900
+    val level1IncomeStart = 44500
+    if (this.earnedIncome < level1IncomeStart) {
+      return minimumNonTaxable
+    }
+    val level1IncomeLimit = 121000
+    val level2IncomeStart = 139100
+    val maximumNonTaxable: Double = 34300
+    if (this.earnedIncome >= level1IncomeLimit && this.earnedIncome < level2IncomeStart) {
+      return maximumNonTaxable
+    }
+    val level2limit = 350100
+    if (this.earnedIncome >= level2limit) {
+      return 13100
+    }
+    if (this.earnedIncome < level1IncomeLimit) {
+      var level1IncomeChange = 500
+      var level1DeductionChange = 100
+      val steps1 = floor((this.earnedIncome - level1IncomeStart) / level1IncomeChange) + 1
+      return minimumNonTaxable + steps1 * level1DeductionChange
+    }
+    if (this.earnedIncome < level2limit) {
+      val level2IncomeChange = 1000
+      val level2DeductionChange = -100
+      val steps2 = floor((this.earnedIncome - level2IncomeStart) / level2IncomeChange) + 1
+      return maximumNonTaxable + steps2 * level2DeductionChange
+    }
+    0
   }
 
   // sjöinkomstavdrag = lake deductions ???
@@ -40,13 +79,6 @@ class TaxableIncome(earnedIncome: Double) {
 
   def calculateSum: Double = {
     this.getTotalIncome - this.getTotalDeductions
-    /*var taxableIncome = 486900
-    if (this.earnedIncome == 300000) {
-      taxableIncome = 281800
-    } else if (this.earnedIncome == 700000) {
-      taxableIncome = 686900
-    }
-    taxableIncome*/
   }
 
   def getJson: JsObject = {
