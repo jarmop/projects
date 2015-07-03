85a36dbe67c856b6af605223921906cb38d85407
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-03 20:07:30 +0300

    fixed tests

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -3,12 +3,13 @@
 describe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
+  beforeEach(module('taxServices'));
 
   describe('view1 controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var view1Ctrl = $controller('View1Ctrl', {$scope: {}});
       expect(view1Ctrl).toBeDefined();
     }));
 
