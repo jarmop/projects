767d2477c011dce9286843c55748f5b30bcc4238
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-05 22:21:06 +0300

    Moved css to app root

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   gulp.src('./app/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css'));
+    .pipe(gulp.dest('./app'));
 });
 gulp.task('sass:watch', function () {
   gulp.watch('./app/scss/*.scss', ['sass']);
