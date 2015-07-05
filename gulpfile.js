21498b9c6040bbf58929b6691609e814b2fab733
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-05 22:11:00 +0300

    Added gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
+'use strict';
+
 var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+  gulp.src('./app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/scss/*.scss', ['sass']);
+});
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-
 gulp.task('serve', function() {
   browserSync({
     server: {
       baseDir: 'app'
     }
   });
-
   gulp.watch(['*.html', 'view1/*', 'view2/*'], {cwd: 'app'}, reload);
 });
