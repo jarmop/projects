d90d159e2da80a39c39acc3ab411fb25be596192
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-06 23:21:32 +0300

    gulp pipeline fun

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,54 @@
 
 var gulp = require('gulp');
 
-var sass = require('gulp-sass');
-gulp.task('sass', function () {
-  gulp.src('./app/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app'));
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/scss/*.scss', ['sass']);
-});
-
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 gulp.task('serve', function() {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: './dist'
     }
   });
-  gulp.watch(['*.html', 'view1/*', 'view2/*', 'scss/*'], {cwd: 'app'}, reload);
+  gulp.watch(['*.html', 'view1/*', 'view2/*', '*.css'], {cwd: 'app'}, ['build', reload]);
 });
+
+//var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+/*var ngmin = require('gulp-ngmin');
+var rev = require('gulp-rev');
+var sourcemaps = require('gulp-sourcemaps');*/
+var destFolder = 'dist';
+
+var mainBowerFiles = require('main-bower-files');
+gulp.task("bower-files", function(){
+  gulp.src(mainBowerFiles('**/*.js'))
+    //.pipe(concat('lib.js'))
+    .pipe(gulp.dest(destFolder + '/lib'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['src/*.js', 'src/controllers/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(destFolder));
+});
+
+var sass = require('gulp-sass');
+gulp.task('styles', function() {
+  gulp.src('src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(destFolder));
+});
+
+gulp.task('html', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest(destFolder));
+  gulp.src('src/templates/*')
+    .pipe(gulp.dest(destFolder + '/templates'));
+});
+
+var del = require('del');
+gulp.task('clean', function () {
+  del('dist')
+});
+
+gulp.task('build', ['clean', 'bower-files', 'scripts', 'styles', 'html']);
--- src/app.js
+++ src/app.js
@@ -3,11 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'ui.bootstrap',
   'taxServices',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  //'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
