90787c50a7763625d208d6caa8fe342b22e31e1d
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-16 08:44:57 +0300

    Clean all before build. Restructured gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,28 @@
 
 var gulp = require('gulp');
 var merge = require('merge-stream');
-
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
+//var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+/*var ngmin = require('gulp-ngmin');
+ var rev = require('gulp-rev');
+ var sourcemaps = require('gulp-sourcemaps');*/
+var destFolder = 'dist';
+var del = require('del');
+var flatten = require('gulp-flatten');
+var sass = require('gulp-sass');
+var mainBowerFiles = require('main-bower-files');
+
 gulp.task('serve', ['build'], function() {
   browserSync({
     server: {
       baseDir: './dist'
     }
   });
-  gulp.watch(['*.html', 'modules/view1/*', 'modules/view2/*', 'scss/*'], {cwd: 'src'}, ['build', browserSync.reload]);
+  return gulp.watch(['*.html', 'modules/view1/*', 'modules/view2/*', 'scss/*'], {cwd: 'src'}, ['build', browserSync.reload]);
 });
 
-//var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-/*var ngmin = require('gulp-ngmin');
-var rev = require('gulp-rev');
-var sourcemaps = require('gulp-sourcemaps');*/
-var destFolder = 'dist';
-var del = require('del');
-
-var mainBowerFiles = require('main-bower-files');
 gulp.task("bower-files", function(){
   /* Using separate del task as a dependency fails for some reason. Therefore performing build now as a callback after del. */
   var js = gulp.src(mainBowerFiles('**/*.js'))
@@ -45,14 +47,12 @@ gulp.task('scripts', ['scripts:clean'], function() {
 gulp.task('styles:clean', function() {
   return del(destFolder + '/app.css')
 });
-var sass = require('gulp-sass');
 gulp.task('styles', ['styles:clean'], function() {
   return gulp.src('src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(destFolder));
 });
 
-var flatten = require('gulp-flatten');
 gulp.task('html:clean', function() {
   return del(destFolder + '/**/*.html')
 });
@@ -66,7 +66,9 @@ gulp.task('html', ['html:clean'], function() {
 });
 
 gulp.task('clean', function () {
-  del(destFolder)
+  return del(destFolder);
 });
 
-gulp.task('build', ['bower-files', 'scripts', 'styles', 'html']);
+gulp.task('build', ['clean'], function() {
+  return runSequence('bower-files', 'scripts', 'styles', 'html');
+});
