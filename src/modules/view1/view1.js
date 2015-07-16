a33664209c7283aab0c8b51021f14fbb88c68c12
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-17 00:02:45 +0300

    Update stack and stack pointer register when first line is ran.

--- src/modules/view1/view1.js
+++ src/modules/view1/view1.js
@@ -10,20 +10,30 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', 'Tax', function($scope, Tax) {
-  $scope.stack = [
-    {address: '0x0012FF6C', value: '0x004012E8'},
-    {address: '0x0012FF68', value: '0x0012FFB8'}
-  ];
+  $scope.stack = {
+    '0x0012FF6C': '0x004012E8',
+    '0x0012FF68': '',
+    '0x0012FF64': '',
+    '0x0012FF60': '',
+    '0x0012FF5C': '',
+    '0x0012FF58': '',
+  };
+
+  $scope.registers = {
+    eax: '0x003435C0',
+    ebp: '0x0012FFB8',
+    esp: '0x0012FF6C'
+  };
 
   $scope.code = [
     {code: 'sub:', description: ''},
-    {code: '00401000  push      ebp', description: 'tälläainen laini'},
+    {code: '00401000  push      ebp', description: 'tälläainen laini', instruction: push, argument: 'ebp'},
     {code: '00401001  mov       ebp,esp', description: ''},
     {code: '00401003  mov       eax,0BEEFh', description: ''},
     {code: '00401008  pop       ebp', description: ''},
     {code: '00401009  ret', description: ''},
     {code: 'main:', description: ''},
-    {code: '00401010  push      ebp', description: ''},
+    {code: '00401010  push      ebp', description: '', instruction: push, argument: 'ebp'},
     {code: '00401011  mov       ebp,esp', description: ''},
     {code: '00401013  call      sub (401000h)', description: ''},
     {code: '00401018  mov       eax,0F00Dh', description: ''},
@@ -39,21 +49,58 @@ angular.module('myApp.view1', ['ngRoute'])
 
   $scope.forward = function() {
     if (linePointer < (runningOrder.length - 1)) {
-      linePointer++;
+      setLinePointer(linePointer + 1);
     }
   };
 
   $scope.backward = function() {
     if (linePointer > 0) {
-      linePointer--;
+      setLinePointer(linePointer - 1);
     }
   };
 
   $scope.setActive = function(lineKey) {
-    linePointer = runningOrder.indexOf(lineKey);
+    setLinePointer(runningOrder.indexOf(lineKey));
   };
 
   $scope.goToStart = function() {
-    linePointer = 0;
+    setLinePointer(0);
   };
+
+  function push(value) {
+    decrementESP();
+    $scope.stack[$scope.registers.esp] = value;
+  }
+
+  function pop(register) {
+    if (register !== undefined) {
+      register.value = $scope.stack[$scope.registers.esp];
+    }
+    incrementESP()
+  }
+
+  function decrementESP() {
+    $scope.registers.esp = '0x00' + (parseInt($scope.registers.esp, 16) - 4).toString(16).toUpperCase();
+  }
+
+  function incrementESP() {
+    $scope.registers.esp = '0x00' + (parseInt($scope.registers.esp, 16) + 4).toString(16).toUpperCase();
+  }
+
+  function isRegister(value) {
+    return value in $scope.registers;
+  }
+
+  function runLine(linePointer) {
+    var line = $scope.code[runningOrder[linePointer]];
+    if (isRegister(line.argument)) {
+      line.argument = $scope.registers[line.argument];
+    }
+    line.instruction(line.argument);
+  }
+
+  function setLinePointer(value) {
+    linePointer = value;
+    runLine(linePointer);
+  }
 }]);
