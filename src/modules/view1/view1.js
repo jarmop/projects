617058836f4ecfe0705c088054dd164300bc7782
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-07-19 11:49:30 +0300

    Show dynamic descriptions for each line.

--- src/modules/view1/view1.js
+++ src/modules/view1/view1.js
@@ -39,36 +39,36 @@ angular.module('myApp.view1', ['ngRoute'])
 
   // linked list, each line points to next instruction... eip?
   $scope.code = {
-    'sub:': {instruction: '', arguments: '', description: ''},
-    '0x00401000': {instruction: 'push', arguments: ['ebp'], description: 'tälläainen laini', nextInstruction: '0x00401001'},
-    '0x00401001': {instruction: 'mov', arguments: ['ebp','esp'], description: '', nextInstruction: '0x00401003'},
-    '0x00401003': {instruction: 'mov', arguments: ['eax','0x0000BEEF'], description: '', nextInstruction: '0x00401008'},
-    '0x00401008': {instruction: 'pop', arguments: ['ebp'], description: '', nextInstruction: '0x00401009'},
-    '0x00401009': {instruction: 'ret', arguments: [], description: ''},
-    'main:': {instruction: '', arguments: [], description: ''},
-    '0x00401010': {instruction: 'push', arguments: ['ebp'], description: '', nextInstruction: '0x00401011'},
-    '0x00401011': {instruction: 'mov', arguments: ['ebp','esp'], description: '', nextInstruction: '0x00401013'},
-    '0x00401013': {instruction: 'call', arguments: ['sub'], description: '', nextInstruction: '0x00401018'},
-    '0x00401018': {instruction: 'mov', arguments: ['eax','0x0000F00D'], description: '', nextInstruction: '0x0040101D'},
-    '0x0040101D': {instruction: 'pop', arguments: ['ebp'], description: '', nextInstruction: '0x0040101E'},
-    '0x0040101E': {instruction: 'ret', arguments: [], description: ''},
+    'sub:': {instruction: '', arguments: ''},
+    '0x00401000': {instruction: 'push', arguments: ['ebp'], nextInstruction: '0x00401001'},
+    '0x00401001': {instruction: 'mov', arguments: ['ebp','esp'], nextInstruction: '0x00401003'},
+    '0x00401003': {instruction: 'mov', arguments: ['eax','0x0000BEEF'], nextInstruction: '0x00401008'},
+    '0x00401008': {instruction: 'pop', arguments: ['ebp'], nextInstruction: '0x00401009'},
+    '0x00401009': {instruction: 'ret', arguments: []},
+    'main:': {instruction: '', arguments: []},
+    '0x00401010': {instruction: 'push', arguments: ['ebp'], nextInstruction: '0x00401011'},
+    '0x00401011': {instruction: 'mov', arguments: ['ebp','esp'], nextInstruction: '0x00401013'},
+    '0x00401013': {instruction: 'call', arguments: ['sub'], nextInstruction: '0x00401018'},
+    '0x00401018': {instruction: 'mov', arguments: ['eax','0x0000F00D'], nextInstruction: '0x0040101D'},
+    '0x0040101D': {instruction: 'pop', arguments: ['ebp'], nextInstruction: '0x0040101E'},
+    '0x0040101E': {instruction: 'ret', arguments: []},
   };
 
   var procedureStart = {sub: '0x00401000'};
-  var currentLine = '';
+  var currentLinePointer = '';
 
   var activeRegisters = [];
   $scope.isActiveRegister = function(name) {
     return activeRegisters.indexOf(name) !== -1;
-  }
+  };
 
   var activeStacks = [];
   $scope.isActiveStack = function(address) {
     return activeStacks.indexOf(address) !== -1;
-  }
+  };
 
   $scope.isActiveLine = function(address) {
-    return address == currentLine;
+    return address == currentLinePointer;
   };
 
   $scope.forward = function() {
@@ -80,12 +80,17 @@ angular.module('myApp.view1', ['ngRoute'])
   };
 
   $scope.setActive = function(linePointer) {
-    currentLine = linePointer;
+    currentLinePointer = linePointer;
   };
 
   $scope.rewind = function() {
     initializeStackAndRegisters();
-    currentLine = '';
+    currentLinePointer = '';
+  };
+
+  var description = '';
+  $scope.getDescription = function() {
+    return description
   };
 
   function decrementESP() {
@@ -101,45 +106,57 @@ angular.module('myApp.view1', ['ngRoute'])
   }
 
   function runNextLine() {
-    currentLine = $scope.registers.eip;
-    var line = $scope.code[currentLine];
+    description = '';
+    currentLinePointer = $scope.registers.eip;
+    var line = $scope.code[currentLinePointer];
     if (line === undefined) { return false; }
     $scope.registers.eip = line.nextInstruction;
     activeRegisters = ['eip'];
     activeStacks = [];
     assembler[line.instruction].apply(this, line.arguments);
+    description += 'EIP points to next instruction.';
   }
 
   function push() {
     decrementESP();
     var value = arguments[0];
+    var valueDescription = value;
     if (isRegister(value)) {
-      activeRegisters.push(value);
-      value = $scope.registers[value];
+      var register = value;
+      value = $scope.registers[register];
+      activeRegisters.push(register);
+      valueDescription = 'value from ' + register.toUpperCase() + ' register (' + value + ')';
     }
     $scope.stack[$scope.registers.esp] = value;
     activeStacks.push($scope.registers.esp);
     activeRegisters.push('esp');
+    description += 'Push ' + valueDescription + ' on the stack. ESP register now points to this value. ';
   }
 
   function pop(register) {
     if (register !== undefined) {
       $scope.registers[register] = $scope.stack[$scope.registers.esp];
       activeRegisters.push(register);
+      description += 'Copy value from top of the stack into ' + register.toUpperCase() + ' register. ';
     }
     activeRegisters.push('esp');
     activeStacks.push($scope.registers.esp);
     incrementESP();
     activeStacks.push($scope.registers.esp);
+    description += 'Point top of the stack (ESP) to previous value. '
   }
 
   function mov() {
     var target = arguments[0];
     var value = arguments[1];
+    var valueDescription = value;
     if (isRegister(value)) {
-      activeRegisters.push(value);
-      value = $scope.registers[value];
+      var register = value;
+      value = $scope.registers[register];
+      activeRegisters.push(register);
+      valueDescription = 'value from ' + register.toUpperCase() + ' register (' + value + ')';
     }
+    description += 'Copy ' + valueDescription + ' into ' + target.toUpperCase() + ' register. ';
     $scope.registers[target] = value;
     activeRegisters.push(target);
   }
