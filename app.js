d57483ee2f581caa3954db4e48c31871929a052e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-08-09 20:01:19 +0300

    Sketching

--- app.js
+++ app.js
@@ -1,4 +1,58 @@
-//var inputs = [];
+angular.module('gateSimulator', [])
+  .controller('truthTableController', function() {
+    var truthTable = this;
+    truthTable.values = [
+      [0,0,0],
+      [0,1,1]
+    ];
+
+
+  })
+  .controller('diagramController', function() {
+    /*this.inputs = {
+      in1: 1
+    };*/
+    this.inputs = inputsi;
+    this.gates = [
+      new Gate('Nand', nand, ['in1', 'in1'])
+    ];
+  });
+
+function nand(inputs) {
+  var output = 0;
+  inputs.some(function(input) {
+    if (input == 0) {
+      output = 1;
+    }
+  });
+  return output;
+}
+
+var inputsi = {
+  'in1': 0
+};
+
+function Gate(name, getValue, inputs) {
+  this.name = name;
+  this.inputs = inputs;
+  var inputti = [];
+  for (key in inputs) {
+    inputti.push(inputsi[inputs[key]]);
+  }
+  this.getValue = getValue(inputti);
+}
+
+/*function Gate(name, minInputs, maxInputs) {
+  this.in = 0;
+  this.out = 0;
+  this.getValue = function(inputs) {
+    return this.out;
+  }
+  this.getAmountOfInputs = function() {
+    return inputs;
+  }
+}*/
+
 function fillInputs(inputs, ia, limit) {
   for (var i=0; i<2; i++) {
     ia.push(i);
@@ -20,19 +74,8 @@ function getTestInputs(limit) {
 var inputs = getTestInputs(2);
 
 for(var i=0; i<inputs.length; i++) {
-  console.log(inputs[i]);
-  console.log(nand(inputs[i]));
-}
-
-
-function nand(inputs) {
-  var output = 0;
-  inputs.some(function(input) {
-    if (input == 0) {
-      output = 1;
-    }
-  });
-  return output;
+  //console.log(inputs[i]);
+  //console.log(nand(inputs[i]));
 }
 
 var not = {
@@ -44,17 +87,8 @@ var not = {
   }
 };
 
-function Gate(name, minInputs, maxInputs) {
-  this.in = 0;
-  this.out = 0;
-  this.getValue = function(inputs) {
-    return this.out;
-  }
-  this.getAmountOfInputs = function() {
-    return inputs;
-  }
-}
 
+/*
 var not = new Gate('not', 1, 1);
 inputs = [];
 for (var i=0; i < not.getAmountOfInputs(); i++) {
@@ -96,6 +130,6 @@ var not = [
     inputs: ['in1', 'in1']
   }
 ];
-
+*/
 
 
--- diagram.js
+++ diagram.js
@@ -1,33 +1,38 @@
-var squareWidth = 100;
-var squareHeight = 100;
-var padding = 50;
 
-var diagram = [
-  [],
-  []
-];
+
+function draw() {
+  var squareWidth = 100;
+  var squareHeight = 100;
+  var padding = 50;
+
+  var diagram = [
+    [],
+    []
+  ];
 
 // First lets create our drawing surface out of existing SVG element
 // If you want to create new surface just provide dimensions
-var paper = Snap("#diagram");
+  var paper = Snap("#diagram");
 //var s = Snap("#svg");
 // Lets create big circle in the middle:
-var nand = paper.rect(150, 150, 100, 100);
+  var nand = paper.rect(150, 150, 100, 100);
 // By default its black, lets change its attributes
-nand.attr({
-  fill: "#bada55",
-  stroke: "#000",
-  strokeWidth: 3
-});
+  nand.attr({
+    fill: "#bada55",
+    stroke: "#000",
+    strokeWidth: 3
+  });
 
-var nand2 = paper.rect(300, 150, 100, 100);
+  var nand2 = paper.rect(300, 150, 100, 100);
 // By default its black, lets change its attributes
-nand2.attr({
-  fill: "#bada55",
-  stroke: "#000",
-  strokeWidth: 3
-});
+  nand2.attr({
+    fill: "#bada55",
+    stroke: "#000",
+    strokeWidth: 3
+  });
 
-paper.line(250, 200, 300, 200).attr({stroke: '#000', strokeWidth: 3});
+  paper.line(250, 200, 300, 200).attr({stroke: '#000', strokeWidth: 3});
 
-paper.text(200, 200, 'NAND');
+  paper.text(200, 200, 'NAND');
+}
+
