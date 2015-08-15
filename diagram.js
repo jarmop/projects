868605b89c57e2943e9ade07c90bc7c8f40a848d
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-08-15 14:30:24 +0300

    drag group

--- diagram.js
+++ diagram.js
@@ -2,36 +2,22 @@ var diagram = new Diagram();
 diagram.drawGrid();
 diagram.draw();
 
-var circle = diagram.paper.rect(60,60,60,60);
-
 var move = function(dx,dy,x,y) {
   dx = Math.round(dx / 20)*20;
   dy = Math.round(dy / 20)*20;
   this.attr({
     transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
   });
-
-  console.log(this.attr('transform'));
 }
 
 var start = function( x, y, ev) {
-  console.log('start');
-  /*if( (typeof x == 'object') && ( x.type == 'touchstart') ) {
-    x.preventDefault();
-    this.data('ox', x.changedTouches[0].clientX );
-    this.data('oy', x.changedTouches[0].clientY );
-  }*/
   this.data('origTransform', this.transform().local );
 }
 var stop = function() {
-  console.log('stop');
 }
 
-/*rect.touchstart( start );
-rect.touchmove( move );
-rect.touchend( stop );*/
-
-circle.drag(move, start, stop )
+elem = diagram.drawGate(60, 60, 'and');
+elem.drag(move, start, stop );
 
 function Diagram() {
   this.paper = Snap("#diagram");
@@ -80,14 +66,15 @@ function Diagram() {
       stroke: "#000",
       strokeWidth: this.strokeWidth
     });
-    this.paper.text(x+20, y+40, name);
-    this.drawLine(x-this.lineLength, y+size/4, x, y+size/4);
-    this.drawLine(x-this.lineLength, y+size/4*3, x, y+size/4*3);
-    this.drawLine(x+size, y+size/2, x+size+this.lineLength, y+size/2);
+    var text = this.paper.text(x+20, y+40, name);
+    var line1 = this.drawLine(x-this.lineLength, y+size/4, x, y+size/4);
+    var line2 = this.drawLine(x-this.lineLength, y+size/4*3, x, y+size/4*3);
+    var line3 = this.drawLine(x+size, y+size/2, x+size+this.lineLength, y+size/2);
+    return this.paper.group(nand, text, line1, line2, line3);
   };
 
   this.drawLine = function(x1, y1, x2, y2) {
-    this.paper.line(x1, y1, x2, y2).attr({stroke: '#000', strokeWidth: this.strokeWidth});
+    return this.paper.line(x1, y1, x2, y2).attr({stroke: '#000', strokeWidth: this.strokeWidth});
   };
 
   this.drawSplit = function(x, y) {
