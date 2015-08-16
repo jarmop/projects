88c5c3dade83f25c95eb5b21c91070a28fb55ba5
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2015-08-16 16:41:48 +0300

    Added bootstrap

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ angular.module('gateSimulator', [])
     ];
 
 
-  })
-  .controller('diagramController', function() {
-    /*this.inputs = {
-      in1: 1
-    };*/
-    this.inputs = inputsi;
-    this.gates = [
-      new Gate('Nand', nand, ['in1', 'in1'])
-    ];
   });
 
 function nand(inputs) {
@@ -27,31 +18,6 @@ function nand(inputs) {
   });
   return output;
 }
-
-var inputsi = {
-  'in1': 0
-};
-
-function Gate(name, getValue, inputs) {
-  this.name = name;
-  this.inputs = inputs;
-  var inputti = [];
-  for (key in inputs) {
-    inputti.push(inputsi[inputs[key]]);
-  }
-  this.getValue = getValue(inputti);
-}
-
-/*function Gate(name, minInputs, maxInputs) {
-  this.in = 0;
-  this.out = 0;
-  this.getValue = function(inputs) {
-    return this.out;
-  }
-  this.getAmountOfInputs = function() {
-    return inputs;
-  }
-}*/
 
 function fillInputs(inputs, ia, limit) {
   for (var i=0; i<2; i++) {
@@ -74,62 +40,7 @@ function getTestInputs(limit) {
 var inputs = getTestInputs(2);
 
 for(var i=0; i<inputs.length; i++) {
-  //console.log(inputs[i]);
-  //console.log(nand(inputs[i]));
+  console.log(inputs[i]);
+  console.log(nand(inputs[i]));
 }
-
-var not = {
-  in: 'in',
-  out: 'out',
-  nand: {
-    in: ['in', 'in'],
-    out: 'out'
-  }
-};
-
-
-/*
-var not = new Gate('not', 1, 1);
-inputs = [];
-for (var i=0; i < not.getAmountOfInputs(); i++) {
-  inputs[i] = [0,1];
-}
-
-output = [];
-for(input in inputs) {
-  for(inp in input) {
-
-  }
-  output.push({
-    input: input,
-    output: not.getValue(input)
-  });
-
-  not.getValue(input);
-}
-
-
-var out = not.getValue(0);
-
-function out(input) {
-  this.input = input;
-  this.getValue = function() {
-    this.input.getValue();
-  }
-}
-
-var not = [
-  {
-    type: 'input',
-    name: 'in1',
-    targets: ['nand1', 'nand1']
-  },
-  {
-    type: 'nand',
-    name: 'nand1',
-    inputs: ['in1', 'in1']
-  }
-];
-*/
-
 
