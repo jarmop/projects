3a920c25b7ebb4709015bd23ff4d3b9eb92441ce
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-04-02 13:25:49 +0300

    Handle year 2014.

--- request.js
+++ request.js
@@ -2,7 +2,6 @@ var request = require('request');
 var cheerio = require('cheerio');
 var json2csv = require('json2csv');
 var fs = require('fs');
-var url = 'http://www.taloustutkimus.fi/tuotteet_ja_palvelut/puolueiden_kannatusarviot/puolueiden-kannatusarviot-2015';
 
 var partySupport = {
   'SDP': [],
@@ -15,35 +14,47 @@ var partySupport = {
   'PERUSS': [],
   'MUU': []
 };
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    $ = cheerio.load(body);
-    $('.content-container table.MsoNormalTable tr').slice(2).each(function (index, element) {
-      var td = $(element).children('td');
-      var partyName = td.eq(0).text();
-      td.slice(3).each(function (index, element) {
-        partySupport[partyName].push({
-          'date': '2015-' + (index + 1),
-          'support': $(element).text().trim().replace(',', '.'),
-        });
+
+getYearlyData(2014);
+
+function getYearlyData(year) {
+  request(getUrl(year), function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      parseHtml(body, year);
+      exportCsv(partySupport);
+    }
+  });
+}
+
+function getUrl(year) {
+  return 'http://www.taloustutkimus.fi/tuotteet_ja_palvelut/puolueiden_kannatusarviot/puolueiden-kannatusarviot-' + year;
+}
+
+function parseHtml(body, year) {
+  $ = cheerio.load(body);
+  $('.content-container table tr').slice(2).each(function (index, element) {
+    var td = $(element).children('td');
+    var partyName = td.eq(0).text();
+    td.slice(3).each(function (index, element) {
+      partySupport[partyName].push({
+        'date': year + '-' + (index + 1),
+        'support': $(element).text().trim().replace(',', '.'),
       });
     });
+  });
+}
 
-    exportCsv(partySupport);
-  }
-});
-
-function exportCsv(data) {
+function exportCsv() {
   var fields = ['date'].concat(Object.keys(partySupport));
   var csvData = [];
 
   var dateData = {};
-  for (var i = 0; i < data['SDP'].length; i++) {
-    dateData[data['SDP'][i].date] = {};
+  for (var i = 0; i < partySupport['SDP'].length; i++) {
+    dateData[partySupport['SDP'][i].date] = {};
   }
 
-  for (var partyName in data) {
-    supportData = data[partyName];
+  for (var partyName in partySupport) {
+    supportData = partySupport[partyName];
     for (var i = 0; i < supportData.length; i++) {
       dateData[supportData[i].date][partyName] = supportData[i].support;
     }
