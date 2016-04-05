f6b661d0f6d1b6d9ec6253cd163adeab4fe5568d
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-04-05 23:24:01 +0300

    Regex hell: parsing unit from csv.

--- data/food.js
+++ data/food.js
@@ -9,18 +9,33 @@ lineReader.on('line', function (line) {
   if (counter === 0) {
     var names = line.split(';');
     names.splice(0,2);
+    // i = 44;
     for (var i=0; i<names.length; i++) {
+      console.log(names[i]);
       data.push({
-        'name': names[i].replace(/[^\s]*$/, '').trim()
+        'name': names[i].replace(/[^\s]*$/, '').trim(),
+        'unit': names[i].match(/[^\(]*$/)[0].replace(')', '')
+        // 'unit': names[i].match(/\((.*?)\)$/)[1]
+        // 'unit': names[i].match(/([.])$/)[0]
+        // 'unit': names[i].match(/[^\s]*$/)[0]
       });
+      // console.log(names[i].split(' '));
+      // console.log(names[i].match(/[\((.*?)\)]$/));
+      // console.log(names[i].match(/\(.*?\)/));
+
+      // console.log(names[i].match(/[^\s]*$/)[0].match(/[^\(\)]+/));
+      // console.log(names[i].match(/[^\(]*$/)[0].replace(')', ''));
+
+
     }
+    // process.exit();
+
   } else {
     var values = line.split(';');
     values.splice(0,2);
     for (var i=0; i<values.length; i++) {
       data[i].value = parseFloat(values[i].replace(' ', '').replace(',','.'));
     }
-    // console.log(data);
   }
   counter++;
 });
