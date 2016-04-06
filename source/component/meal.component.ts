9515e41ee5f7ed30242842304eacc1d22388af4d
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-04-06 22:44:38 +0300

    chartjs test

--- source/component/meal.component.ts
+++ source/component/meal.component.ts
@@ -4,6 +4,8 @@ import {RouteParams} from 'angular2/router';
 // import {MealService} from '../service/meal.service';
 // import {Meal} from "../model/meal";
 
+declare var Chart: any;
+
 @Component({
     selector: 'meal',
     templateUrl: 'component/meal.component.html',
@@ -20,6 +22,37 @@ export class MealComponent implements OnInit {
     ngOnInit() {
         // let id = +this._routeParams.get('id');
         // this._mealService.getMeal(id).then(meal => this.meal = meal);
+
+        var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+        var barChartData = {
+            labels : ["January","February","March","April","May","June","July"],
+            datasets : [
+                {
+                    fillColor : "rgba(220,220,220,0.5)",
+                    strokeColor : "rgba(220,220,220,0.8)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                },
+                {
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,0.8)",
+                    highlightFill : "rgba(151,187,205,0.75)",
+                    highlightStroke : "rgba(151,187,205,1)",
+                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                }
+            ]
+        }
+        // window.onload = function(){
+            // var ctx = document.getElementById("canvas").getContext("2d");
+
+            var canvas : any = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
+
+            var myBar = new Chart(ctx).Bar(barChartData, {
+                responsive : true
+            });
+        // }
     }
 
     goBack() {
