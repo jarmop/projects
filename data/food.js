706bf38299c4da78fa9546945027b684b4c23e8e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-04-09 18:24:14 +0300

    Read csv from url.

--- data/food.js
+++ data/food.js
@@ -1,48 +1,45 @@
 var fs = require('fs');
-var lineReader = require('readline').createInterface({
-  input: fs.createReadStream('soybean.csv'/*, {'start' : 50}*/)
-});
+var request = require('request');
 
 var data = [];
-var counter = 0;
-lineReader.on('line', function (line) {
-  if (counter === 0) {
-    var names = line.split(';');
-    names.splice(0,2);
-    // i = 44;
-    for (var i=0; i<names.length; i++) {
-      console.log(names[i]);
-      data.push({
-        'name': names[i].replace(/[^\s]*$/, '').trim(),
-        'unit': names[i].match(/[^\(]*$/)[0].replace(')', '')
-        // 'unit': names[i].match(/\((.*?)\)$/)[1]
-        // 'unit': names[i].match(/([.])$/)[0]
-        // 'unit': names[i].match(/[^\s]*$/)[0]
-      });
-      // console.log(names[i].split(' '));
-      // console.log(names[i].match(/[\((.*?)\)]$/));
-      // console.log(names[i].match(/\(.*?\)/));
 
-      // console.log(names[i].match(/[^\s]*$/)[0].match(/[^\(\)]+/));
-      // console.log(names[i].match(/[^\(]*$/)[0].replace(')', ''));
-
-
-    }
-    // process.exit();
-
-  } else {
-    var values = line.split(';');
-    values.splice(0,2);
-    for (var i=0; i<values.length; i++) {
-      data[i].value = parseFloat(values[i].replace(' ', '').replace(',','.'));
-    }
+request('https://fineli.fi/fineli/fi/elintarvikkeet/391/resultset.csv', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var lines = body.split('\r\n');
+    parseNamesAndUnit(getValues(lines[0]));
+    parseValues(getValues(lines[1]));
+    // console.log(data);
+    saveJson(data);
   }
-  counter++;
 });
 
-lineReader.on('close', function (line) {
+function parseNamesAndUnit(titles) {
+  // i = 44;
+  for (var i=0; i<titles.length; i++) {
+    // console.log(names[i]);
+    data.push({
+      'name': titles[i].replace(/[^\s]*$/, '').trim(),
+      'unit': titles[i].match(/[^\(]*$/)[0].replace(')', '')
+    });
+  }
+  // process.exit();
+}
+
+function parseValues(values) {
+  for (var i=0; i<values.length; i++) {
+    data[i].value = parseFloat(values[i].replace(' ', '').replace(',','.'));
+  }
+}
+
+function getValues(line) {
+  var names = line.split(';');
+  names.splice(0,2);
+  return names;
+}
+
+function saveJson() {
   fs.writeFile('soybean.json', JSON.stringify(data, null, 4), function(err) {
     if (err) throw err;
     console.log('JSON file saved!');
   });
-});
+};
