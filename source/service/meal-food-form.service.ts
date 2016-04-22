9d5ad19ee0dd1ceae7fcfd0e415307096ee3cd3f
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-04-22 19:59:06 +0300

    Get bloodhound data from foods service.

--- source/service/meal-food-form.service.ts
+++ source/service/meal-food-form.service.ts
@@ -1,21 +1,33 @@
 import {Injectable} from 'angular2/core';
+import {FoodService} from 'service/food.service';
 
 declare var Bloodhound:any;
 
 @Injectable()
 export class MealFoodFormService {
     private bloodhound;
-    private foods = ['banaani','porkkana','puuro','peruna','papu'];
+    private mapFoodNameToId: Object = {};
 
-    constructor() {
-        this.bloodhound = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.whitespace,
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: this.foods
+    constructor(private _foodService: FoodService) {
+        this._foodService.getFoods().then(foods => {
+            this.bloodhound = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.whitespace,
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                local: this.getFoodNames(foods)
+            });
         });
     }
 
     getTypeaheadSource() {
         return this.bloodhound;
+    }
+
+    getFoodNames(foods) {
+        let foodNames = [];
+        for (let food of foods) {
+            this.mapFoodNameToId[food.name] = food.id;
+            foodNames.push(food.name);
+        }
+        return foodNames;
     }
 }
