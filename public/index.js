ac0a885740dea120b4f0d1fb8790e44050e10b09
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-07-11 11:20:31 +0300

    Draw tree.

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
 var config = {
-  nodeHeight: 30,
+  nodeSize: 30,
   strokeWidth: 3,
-  nodeDistance: 10
+  nodeDistance: 10,
+  treeNodeDistanceX: 10
 };
 
 var draw = SVG('drawing').size(600, 600);
 
 function drawNode(x, y, value) {
-  var circle = draw.circle(config.nodeHeight).attr({
+  var circle = draw.circle(config.nodeSize).attr({
     fill: '#fff',
     stroke: '#000',
     'stroke-width': config.strokeWidth
@@ -32,31 +33,31 @@ function drawNode(x, y, value) {
 
 function drawLink(node1, node2) {
   console.log(node1.x());
-  var x1 = node1.x() + config.nodeHeight / 2;
-  var y1 = node1.y() + config.nodeHeight / 2;
-  var x2 = node2.x() + config.nodeHeight / 2;
-  var y2 = node2.y() + config.nodeHeight / 2;
+  var x1 = node1.x() + config.nodeSize / 2;
+  var y1 = node1.y() + config.nodeSize / 2;
+  var x2 = node2.x() + config.nodeSize / 2;
+  var y2 = node2.y() + config.nodeSize / 2;
   draw.line(x1,y1,x2,y2).stroke({width: config.strokeWidth});
 }
 
 function drawData(data) {
-  var x1 = 10 + config.nodeHeight / 2;
-  var y1 = 10 + config.nodeHeight / 2;
-  var x2 = x1 + config.nodeDistance + config.nodeHeight;
+  var x1 = 10 + config.nodeSize / 2;
+  var y1 = 10 + config.nodeSize / 2;
+  var x2 = x1 + config.nodeDistance + config.nodeSize;
   var y2 = y1;
   for (var i = 1; i < data.length; i++) {
     draw.line(x1,y1,x2,y2).stroke({width: config.strokeWidth});
     drawNode(
-      x1 - config.nodeHeight / 2,
-      y1 - config.nodeHeight / 2,
+      x1 - config.nodeSize / 2,
+      y1 - config.nodeSize / 2,
       data[i - 1]
     );
     x1 = x2;
-    x2 = x1 + config.nodeDistance + config.nodeHeight;
+    x2 = x1 + config.nodeDistance + config.nodeSize;
   }
   drawNode(
-    x1 - config.nodeHeight / 2,
-    y1 - config.nodeHeight / 2,
+    x1 - config.nodeSize / 2,
+    y1 - config.nodeSize / 2,
     data[i - 1]
   );
 }
@@ -71,3 +72,31 @@ for (var i = 0; i < 10; i++) {
 }
 
 drawData(data);
+
+function drawTree(data) {
+  var levels = 4;
+  var nodeAmountOnLastLevel = Math.pow(2, levels - 1);
+  var width = nodeAmountOnLastLevel * config.nodeSize + (nodeAmountOnLastLevel - 1) * config.treeNodeDistanceX;
+  console.log(width);
+  var i = 0;
+  var x;
+  var j;
+  var y = 50;
+  for (var level = 0; level < 4; level++) {
+    var wut = Math.pow(2, levels - level - 1);
+    x = (config.nodeDistance * wut + config.nodeSize * wut) / 2;
+    console.log(x);
+    var dist = (config.nodeDistance * wut + config.nodeSize * wut);
+    j = 0;
+    while (j < Math.pow(2, level) && i < data.length) {
+      drawNode(x, y, data[i]);
+      // x += config.treeNodeDistanceX + config.nodeSize;
+      x += dist;
+      i++;
+      j++
+    }
+    y += config.nodeDistance + config.nodeSize;
+  }
+}
+
+drawTree(data);
