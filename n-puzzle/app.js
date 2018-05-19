7996f74ce729736493998e7ec586f2137bc76158
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-19 20:53:20 +0300

    Move adjacent block to empty grid on click.

--- n-puzzle/app.js
+++ n-puzzle/app.js
@@ -1,7 +1,32 @@
+let emptyGrid = {column: 4, row: 4};
+
+const moveBlock = (event) => {
+  let grid = {
+    column: parseInt(event.target.style['grid-column']),
+    row: parseInt(event.target.style['grid-row']),
+  };
+
+  if (Math.abs(emptyGrid.column - grid.column) + Math.abs(emptyGrid.row - grid.row) < 2) {
+    event.target.style = 'grid-column: ' + emptyGrid.column + '; grid-row: ' + emptyGrid.row;
+    emptyGrid = grid;
+  }
+};
+
 let gameBoard = document.querySelector('.gameboard');
+let row = 1;
+let column = 1;
 for (let i = 1; i < 16; i++) {
   let block = document.createElement('div');
+  block.dataset.id = i;
   block.className = 'block';
+  block.style = 'grid-column: ' + column + '; grid-row: ' + row;
   block.appendChild(document.createTextNode(i));
+  block.onclick = moveBlock;
   gameBoard.appendChild(block);
+  if (i % 4 === 0) {
+    row++;
+    column = 1;
+  } else {
+    column++;
+  }
 }
