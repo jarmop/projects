3438cf29deeaddb59f1fcf4fcf0f654866431cfe
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-11-24 17:29:24 +0200

    Move object with gravity.

--- gravity/src/App.js
+++ gravity/src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 const CANVAS_WIDTH = 200;
 const CANVAS_HEIGHT = 300;
 const OBJECT_RADIUS = 10;
+const OBJECT_ORIGINAL_Y = 90;
+const TIME_INCREMENT_MS = 100;
+const G = 9.81;
 
 function App() {
-  const [y, setY] = useState(100);
+  const [state, setState] = useState({
+    y: OBJECT_ORIGINAL_Y,
+    t: 0,
+  });
   const [timeIsRunning, setTimeIsRunning] = useState(false);
   useEffect(() => {
     const canvas = document.getElementById('canvas');
@@ -14,18 +20,27 @@ function App() {
 
     c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     c.beginPath();
-    c.arc(CANVAS_WIDTH / 2, y, OBJECT_RADIUS, 0, Math.PI*2);
+    c.arc(CANVAS_WIDTH / 2, state.y, OBJECT_RADIUS, 0, Math.PI*2);
     c.fill();
     if (timeIsRunning) {
-      tick();
+      if (state.y + OBJECT_RADIUS === CANVAS_HEIGHT) {
+        setTimeIsRunning(false);
+      } else {
+        tick();
+      }
     }
   });
 
   const tick = () => {
-    console.log('tick');
     setTimeout(() => {
-      setY(Math.min(y + 10, CANVAS_HEIGHT - OBJECT_RADIUS));
-    }, 1000);
+      const newTime = state.t + TIME_INCREMENT_MS / 1000;
+      const d = G * Math.pow(newTime, 2) / 2;
+      console.log('t: ' + newTime + ', d: ' + d);
+      setState({
+        y: Math.min(OBJECT_ORIGINAL_Y + d, CANVAS_HEIGHT - OBJECT_RADIUS),
+        t: newTime,
+      });
+    }, TIME_INCREMENT_MS);
   };
 
   return (
@@ -34,7 +49,7 @@ function App() {
         <div>
           <button onClick={() => setTimeIsRunning(!timeIsRunning)}>{timeIsRunning ? 'Stop' : 'Drop'}</button>
           <div>
-            y: {y}
+            Height: {CANVAS_HEIGHT - state.y - OBJECT_RADIUS} m
           </div>
         </div>
       </div>
