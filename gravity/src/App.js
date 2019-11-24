f6353ad0c64440c2815c77bc501676102679ac58
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-11-24 16:33:01 +0200

    Move object with button click, prevent crossing border.

--- gravity/src/App.js
+++ gravity/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const CANVAS_WIDTH = 200;
+const CANVAS_HEIGHT = 300;
+const OBJECT_RADIUS = 10;
+
 function App() {
+  const [y, setY] = useState(100);
+  useEffect(() => {
+    const canvas = document.getElementById('canvas');
+    const c = canvas.getContext('2d');
+
+    c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    c.beginPath();
+    c.arc(CANVAS_WIDTH / 2, y, OBJECT_RADIUS, 0, Math.PI*2);
+    c.fill();
+  });
+
   return (
-    <div className="App">
-      Gravity
-    </div>
+      <div className="app">
+        <canvas id="canvas" className="canvas" width={CANVAS_WIDTH} height={CANVAS_HEIGHT}/>
+        <div>
+          <button onClick={() => setY(Math.min(y + 10, CANVAS_HEIGHT - OBJECT_RADIUS))}>Move</button>
+          <div>
+            y: {y}
+          </div>
+        </div>
+      </div>
   );
 }
 
