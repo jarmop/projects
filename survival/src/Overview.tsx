d20bf2a200a97889892b47cc68c75c2e7187f4bc
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2026-08-11 13:35:27 +0300

    Survival: Starvation

--- survival/src/Overview.tsx
+++ survival/src/Overview.tsx
@@ -1,83 +1,110 @@
-import { type JSX, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const portionOfWomen = 0.5;
-const birthPerWoman = 2;
-const lifeExpectancy = 100;
-
-const population: number[] = [];
-for (let i = 0; i < lifeExpectancy; i++) {
-  population.push(10);
-}
+const ageOfNaturalDeath = 100;
 
 const defaultSociety = {
-  population: 1000,
-  birthPerWoman: birthPerWoman, // every woman gives birth once for every ten years they live
-  lifeExpectancy: lifeExpectancy,
+  population: [] as number[],
+  birthPerWoman: 2, // every woman gives birth once for every ten years they live
+  lifeExpectancy: ageOfNaturalDeath,
   birthRate: 0,
   deathRate: 0,
   populationGrowthRate: 0,
 };
 
-defaultSociety.birthRate = defaultSociety.population * portionOfWomen *
-  defaultSociety.birthPerWoman /
-  defaultSociety.lifeExpectancy;
+for (let i = 0; i < defaultSociety.lifeExpectancy; i++) {
+  defaultSociety.population.push(10);
+}
+defaultSociety.birthRate = sum(defaultSociety.population) * portionOfWomen *
+  defaultSociety.birthPerWoman / defaultSociety.lifeExpectancy;
 defaultSociety.deathRate = 1000 / defaultSociety.lifeExpectancy;
 defaultSociety.populationGrowthRate =
   (defaultSociety.birthRate - defaultSociety.deathRate) / 1000;
 
 const humanWaterNeedPerDay = 3;
 const humanCalorieNeedPerDay = 3000;
-const wheatCalories = 3500;
+const wheatCalories = 3000;
+const humanCalorieNeedPerYear = humanCalorieNeedPerDay * 365;
+
+const wheatProductionPerYear = humanCalorieNeedPerYear * 999 / wheatCalories;
 
 export function Overview() {
   const [society, setSociety] = useState(defaultSociety);
+  const totalPopulation = sum(society.population);
   const [year, setYear] = useState(0);
+  const [wheatStorage, setWheatStorage] = useState(
+    humanCalorieNeedPerYear * totalPopulation /
+      wheatCalories,
+  );
+  const wheatDemandPerYear = humanCalorieNeedPerYear * totalPopulation /
+    wheatCalories;
+
+  const calorieSupply = wheatStorage * wheatCalories;
+  const populationCalorieNeedPerYear = humanCalorieNeedPerYear *
+    totalPopulation;
+  const populationCalorieBalance = calorieSupply - populationCalorieNeedPerYear;
+
+  const starvingPeople = Math.min(
+    Math.floor(-populationCalorieBalance / humanCalorieNeedPerYear),
+  );
 
   function increaseYear() {
     const increase = 1;
     const newYear = year + increase;
     setYear(newYear);
 
-    const women = society.population * portionOfWomen;
-    const births = society.birthPerWoman / society.lifeExpectancy * women;
-    const deaths = society.population / lifeExpectancy;
-    console.log(births, deaths);
+    const women = Math.ceil(totalPopulation * portionOfWomen);
+    const births = Math.ceil(
+      society.birthPerWoman / society.lifeExpectancy * women,
+    );
+    let deaths = society.population[ageOfNaturalDeath - 1];
+    let sumOfAgesOfDying = deaths * ageOfNaturalDeath;
 
-    // But the demographic is not going to stay even if the population is growing.
-    // Every year there are more people giving birth. The number of deaths per year
-    // lags 100 years behind the number of births.
+    const newPopulation = [births, ...society.population].slice(0, -1);
+
+    let peopleToStarve = starvingPeople;
+
+    for (
+      let age = newPopulation.length - 1;
+      age >= 0 && peopleToStarve > 0;
+      age--
+    ) {
+      if (peopleToStarve >= newPopulation[age]) {
+        peopleToStarve -= newPopulation[age];
+        deaths += newPopulation[age];
+        sumOfAgesOfDying += newPopulation[age] * age;
+        newPopulation[age] = 0;
+      } else {
+        newPopulation[age] -= peopleToStarve;
+        deaths += peopleToStarve;
+        sumOfAgesOfDying += peopleToStarve * age;
+        peopleToStarve = 0;
+      }
+    }
+    // console.log(births, deaths);
 
     const newSociety = {
       ...society,
-      population: society.population *
-        Math.pow(1 + society.populationGrowthRate, increase),
+      lifeExpectancy: sumOfAgesOfDying / deaths,
+      population: newPopulation,
     };
 
-    // newSociety.population = society.population +
-    // society.population * society.populationGrowthRate;
-
     setSociety(newSociety);
+
+    setWheatStorage(
+      Math.max(0, wheatStorage - wheatDemandPerYear) + wheatProductionPerYear,
+    );
   }
-
-  const populationPerAge: JSX.Element = [];
-
-  // for (let i = 0;)
-  // return (
-  //   <tr>
-  //     <th>0-9:</th>
-  //     <td>{society.population / 100}</td>
-  //   </tr>
-  // );
 
   return (
     <div>
-      <h3>Overview</h3>
+      <h3>Stats</h3>
       <table>
         <tbody>
           <tr>
             <th>Population:</th>
-            <td>{society.population}</td>
+            <td>{totalPopulation}</td>
           </tr>
           <tr>
             <th>Births per woman:</th>
@@ -89,7 +116,7 @@ export function Overview() {
           </tr>
           <tr>
             <th>Life expectancy:</th>
-            <td>{society.lifeExpectancy} years</td>
+            <td>{society.lifeExpectancy.toFixed(1)} years</td>
           </tr>
           <tr>
             <th>Death rate:</th>
@@ -102,32 +129,30 @@ export function Overview() {
         </tbody>
       </table>
 
-      <h3>Population per age</h3>
+      {
+        /* <h3>Population per age</h3>
       <table>
         <tbody>
           <tr>
             <th>Fertile (20-29):</th>
-            <td>{society.population / 100}</td>
+            <td>{sum(society.population.slice(20, 30))}</td>
           </tr>
         </tbody>
-      </table>
+      </table> */
+      }
 
       <h3>Needs per person per day</h3>
       <table>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Demand</th>
-            <th>Supply</th>
+            <th>Amount</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
               Water (l)
-            </td>
-            <td>
-              {humanWaterNeedPerDay}
             </td>
             <td>
               {humanWaterNeedPerDay}
@@ -140,42 +165,77 @@ export function Overview() {
             <td>
               {humanCalorieNeedPerDay}
             </td>
-            <td>
-              {humanCalorieNeedPerDay}
-            </td>
           </tr>
         </tbody>
       </table>
 
-      <h3>Production Per year</h3>
+      <h3>Expected production this year</h3>
       <table>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Demand</th>
-            <th>Supply</th>
+            {/* <th>Demand</th> */}
+            <th>Amount</th>
+            {/* <th>Balance</th> */}
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-              Grain (tons)
+              Grain (kg)
+            </td>
+            {
+              /* <td>
+              {wheatDemandPerYear.toFixed(0)}
+            </td> */
+            }
+            <td>
+              {wheatProductionPerYear.toFixed(0)}
+            </td>
+            {
+              /* <td>
+              {(wheatProductionPerYear - wheatDemandPerYear).toFixed(0)}
+            </td> */
+            }
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Storage compared to consumption</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Amount</th>
+            <th>Consumption</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Grain (kg)
             </td>
             <td>
-              {(humanCalorieNeedPerDay * 365 * society.population /
-                wheatCalories /
-                1000)
-                .toFixed(0)}
+              {wheatStorage.toFixed(0)}
             </td>
             <td>
-              {(humanCalorieNeedPerDay * 365 * society.population /
-                wheatCalories /
-                1000)
-                .toFixed(0)}
+              {wheatDemandPerYear.toFixed(0)}
+            </td>
+            <td>
+              {(wheatStorage - wheatDemandPerYear).toFixed(0)}
             </td>
           </tr>
         </tbody>
       </table>
+
+      <h3>Population calorie balance</h3>
+      {populationCalorieBalance}
+
+      <p>
+        Deficit of N human's yearly calorie needs will result in N deaths.
+      </p>
+      Amount of people who will starve to death: {starvingPeople}
 
       <div style={{ position: "absolute", top: 0, right: 0, display: "flex" }}>
         <table>
@@ -190,4 +250,8 @@ export function Overview() {
       </div>
     </div>
   );
+}
+
+function sum(values: number[]) {
+  return values.reduce((total, n) => total + n, 0);
 }
